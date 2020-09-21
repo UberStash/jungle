a4eebf53fbd72005020dfe4e2115c5bcf4b60038
UberStash <expiredserver@gmail.com>
2020-09-21 22:19:37 +0000

    had to change around login and signup method as neglected to but first and last name and just put name instead then had to go through the login credentials and make a method to make the test pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
   describe 'Validations' do
-  # validates: name, presence: true
-  # validates price, presence true
-  # validates quantity, presence true
-  # validates category, presence true
+
   cat = Category.find_or_create_by! name: 'Furniture'
 
   subject {
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "Is valid with all attributes" do
     @user = User.create(
-      name: "John McLellan",
+      first_name: "John",
+      last_name: "McLellan",
       email: "test@test.com",
       password: "password",
       password_confirmation: "password")
@@ -12,38 +13,71 @@ RSpec.describe User, type: :model do
 
   it "Is not valid without password" do
     @user = User.create(
-      name: "John McLellan",
-      email: "test@test.com",
-      password: "password",
-      password_confirmation: "Password")
-    expect(@user).not_to  be_valid   
-  end
-
-  it "Is not valid without name" do
-    @user = User.create(
-      name: "",
+      first_name: "John",
+      last_name: "McLellan",
       email: "test@test.com",
       password: nil,
       password_confirmation: "password")
     expect(@user).not_to  be_valid   
   end
 
-  it "Is not valid without email" do
+  it "Is not valid without name" do
     @user = User.create(
-      name: "Happy",
-      email: nil,
+      first_name: nil,
+      last_name: "McLellan",
+      email: "test@test.com",
       password: "password",
-      password_confirmation: "Password")
+      password_confirmation: "password")
     expect(@user).not_to  be_valid   
   end
 
-  it "Is not valid without name" do
+  it "Is not valid without email" do
     @user = User.create(
-      name: nil,
-      email: "test@test.com",
+      first_name: "John",
+      last_name: "McLellan",
+      email: nil,
       password: "password",
-      password_confirmation: "Password")
+      password_confirmation: "password")
     expect(@user).not_to  be_valid   
+  end
+
+
+  it "Password length should be >= 5" do
+    @user = User.create(
+      first_name: "John",
+      last_name: "McLellan",
+      email: "test@test.com",
+      password: "secure",
+      password_confirmation: "secure")
+    expect(@user.password.length).to be >= 5
+  end
+
+  it "Passwords won't match" do
+    @user = User.create(
+      first_name: "John",
+      last_name: "McLellan",
+      email: "test@test.com",
+      password: "monster",
+      password_confirmation: "mash")
+    expect(@user.errors[:password_confirmation].first).to eq ("doesn't match Password")
+  end
+
+end
+
+
+
+describe '.authenticate_with_credentials' do
+
+it "Should authenticate even with spaces before/after email" do
+    @user = User.create(
+      first_name: "John",
+      last_name: "McLellan",
+      email: "this@gmail",
+      password: "secure",
+      password_confirmation: "secure")
+    @login = User.authenticate_with_credentials("this@gmail", "secure")  
+    expect(@login).not_to be_nil
+    
   end
 
 
