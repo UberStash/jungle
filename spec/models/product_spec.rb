615cd48f11eb916192e158285f724e5b15b42b2a
UberStash <expiredserver@gmail.com>
2020-09-21 18:47:13 +0000

    completed product test examples

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,19 +10,35 @@ RSpec.describe Product, type: :model do
   cat = Category.find_or_create_by! name: 'Furniture'
 
   subject {
-    described_class.new(name: "Product name",
-                        description: "Product description",
-                        image: 'apparel1.jpg',
-                        price_cents: 400,
+    described_class.new(name: "Name",
+                        price_cents: 1000,
                         quantity: 1,
                         category: cat)
   }
 
-    it "Is valid with all attributes" do
+    it "Is valid with all attributes passed in" do
       expect(subject).to be_valid
     end
 
-    
+    it "should have a name" do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "should have a price" do
+      subject.price_cents = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "should have a quanitity" do
+      subject.quantity = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "should have a category" do
+      subject.category = nil
+      expect(subject).not_to be_valid
+    end
 
   end
 end
